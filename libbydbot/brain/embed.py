3579af9d17ad7b759341b71c82408faddd6b6498
Flávio Codeço Coelho <fccoelho@gmail.com>
2024-04-11 17:42:42 -0300

    Fixed the embed module so that it can answer questions based on PDFs

--- libbydbot/brain/embed.py
+++ libbydbot/brain/embed.py
@@ -1,52 +1,83 @@
 import ollama
-import chromadb
+# import chromadb
+import os
 import pgvector
-from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy import Column, Integer, String, Text
+from hashlib import sha256
+from sqlalchemy.orm import DeclarativeBase, Session
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy import Column, Integer, String, text, create_engine, select
 from pgvector.sqlalchemy import Vector
 import dotenv
+import loguru
 
 dotenv.load_dotenv()
+logger = loguru.logger
+
+engine = create_engine(os.getenv("PGURL"))
+with Session(engine) as session:
+    session.execute(text('CREATE EXTENSION IF NOT EXISTS vector;'))
 
 # create a class to store the embeddings
 class Base(DeclarativeBase):
     pass
-class Embedding(Base):
+
+
+class DocVector(Base):
     __tablename__ = "embeddings"
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    doc_name = Column(String)
+    page_number = Column(Integer)
+    hash = Column(String, unique=True)
     document = Column(String)
-    embedding = Column(Vector)
+    embedding = Column(Vector(1024))
 
+class DocEmbedder:
+    def __init__(self):
+        self.engine = create_engine(os.getenv("PGURL"))
+        self.session = Session(self.engine)
+        Base.metadata.create_all(self.engine)
 
-
-
-
-def embed_docs(documents):
-    # store each document in a vector embedding database
-    for i, d in enumerate(documents):
-        response = ollama.embeddings(model="mxbai-embed-large", prompt=d)
+    def embed_text(self, doctext: object, docname:str, page_number: object) -> object:
+        response = ollama.embeddings(model="mxbai-embed-large", prompt=doctext)
         embedding = response["embedding"]
-        collection.add(
-            ids=[str(i)],
-            embeddings=[embedding],
-            documents=[d]
+        # print(len(embedding))
+        docv = DocVector(
+            hash=sha256(doctext.encode()).hexdigest(),
+            doc_name=docname,
+            page_number=page_number,
+            document=doctext,
+            embedding=embedding)
+        try:
+            self.session.add(docv)
+            self.session.commit()
+        except IntegrityError:
+            self.session.rollback()
+            logger.warning(f"Document {docname} page {page_number} already exists in the database.")
+
+
+    def retrieve_docs(self, query):
+        response = ollama.embeddings(model="mxbai-embed-large", prompt=query)
+        statement = (
+            select(DocVector.document)
+            .order_by(DocVector.embedding.l2_distance(response["embedding"]))
         )
+        data = self.session.scalars(statement).first()
+        return data
 
-def retrieve_docs(query):
-    response = ollama.embeddings(model="mxbai-embed-large", prompt=query)
-    results = collection.query(
-        query_embeddings=[response["embedding"]],
-        n_results=1
-    )
-    data = results['documents'][0][0]
-    return data
+    def generate_response(self, question):
+        context = self.retrieve_docs(question)
+        # print(context)
+        response = ollama.generate(
+            model="gemma",
+            prompt=f"Using this context: {context} \n\nAnswer this question: {question}",
+            system=context
+        )
+        return response["response"]
 
-def generate_response(question):
-    context = retrieve_docs(question)
-    print(context)
-    response = ollama.generate(
-        model="gemma",
-        prompt=f"Using this context: {context} \n\nAnswer this question: {question}",
-        system=context
-    )
-    return response["response"]
+    def __del__(self):
+        self.session.close()
+
+
+
+if __name__ == '__main__':
+    embed_text()
--- libbydbot/cli.py
+++ libbydbot/cli.py
@@ -7,13 +7,15 @@ from glob import glob
 
 
 def main(corpus_path='.'):
+    DE = embed.DocEmbedder()
     print ("Processing your corpus...")
     for d in glob(os.path.join(corpus_path, '*.pdf')):
         doc = fitz.open(d)
-        for page in doc:
-            embed.embed_docs(page.get_text())
+        n = doc.name
+        for page_number, page in enumerate(doc):
+            DE.embed_text(page.get_text(), n, page_number)
     question = input("Ask me a question: ")
-    response = embed.generate_response(question)
+    response = DE.generate_response(question)
     print(response)
 
 if __name__ == '__main__':
