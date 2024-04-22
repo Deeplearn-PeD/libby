5f431b1198dfbbbe3dd03a3d1862ea55bb2a76dc
Flávio Codeço Coelho <fccoelho@gmail.com>
2024-04-22 14:26:17 -0300

    Updated cli

--- libbydbot/cli.py
+++ libbydbot/cli.py
@@ -28,17 +28,7 @@ class LibbyInterface:
 
 
 def main(corpus_path='.'):
-    DE = embed.DocEmbedder()
-    print ("Processing your corpus...")
-    for d in glob(os.path.join(corpus_path, '*.pdf')):
-        doc = fitz.open(d)
-        n = doc.name
-        for page_number, page in enumerate(doc):
-            DE.embed_text(page.get_text(), n, page_number)
-    question = input("Ask me a question: ")
-    response = DE.generate_response(question)
-    print(response)
-
-if __name__ == '__main__':
     fire.Fire(LibbyInterface)
+
+
 
